d4e7d364c968b698b44ebdff4ae15531a5d7f0f3
Your Name <you@example.com>
2022-02-24 11:48:08 +0530

    commited

---
+++ collection1/src/main/java/collection1/collectionDemo.java
@@ -0,0 +1,14 @@
+package collection1;
+import java.util.ArrayList;
+public class collectionDemo {
+
+    public static void main(String[] args){
+        ArrayList<String> animals = new ArrayList<String>();
+        // Add elements
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Horse");
+
+        System.out.println("ArrayList: " + animals);
+    }
+}
